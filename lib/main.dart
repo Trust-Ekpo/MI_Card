1a3ee1e643ec0889fd9dd7e386da871119e9f893
Trust Ekpo <tekpo24@gmail.com>
2020-09-06 14:53:31 +0100

    change some content

--- lib/main.dart
+++ lib/main.dart
@@ -64,7 +64,7 @@ class MyApp extends StatelessWidget {
                        ),
 
                        title: Text(
-                       " +234 8097700177",
+                       " +234 8000000100",
                          style: TextStyle(
                            fontSize: 15
                          ),
